e9d986633991dd0ecca30f8500c26c02f32011b7
Nguyen Van Thinh <nguyenvanthinh0817@gmail.com>
2020-12-21 00:00:35 +0700

    Fix bug add coin

--- src/hooks/refill-coins/add-coins.js
+++ src/hooks/refill-coins/add-coins.js
@@ -1,12 +1,23 @@
 module.exports = (context) => {
   const { result } = context;
   const { userId, amount, status } = result;
+  console.log(context.params);
   if (status == 2) {
     const coin = amount / 1000;
-    context.app.services.users.patch(userId, { coin }, {}).then((res) => {
-      console.log("Nap tien thanh cong");
-      return context;
-    });
+    context.app.services.users
+      .get(userId, context.params)
+      .then((res) => {
+        const totalCoin = res.coin + coin;
+        context.app.services.users
+          .patch(userId, { coin: totalCoin }, {})
+          .then((res) => {
+            console.log("Nap tien thanh cong");
+            return context;
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   } else {
     console.log("Da tu choi");
     return context;
